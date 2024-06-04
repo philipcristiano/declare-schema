51eebccb35146de7e4a727d016977c5f56a78b77
Philip Cristiano <git@philipcristiano.com>
2024-06-04 14:07:11 -0400

    lib: `migrate_from_string` fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
 pub mod altertable;
 pub mod schema;
 pub mod source_postgres;
+
+use sqlx::PgPool;
+use altertable::Wrapped;
+
+pub async fn migrate_from_string(src: &String, pool: &PgPool) -> anyhow::Result<()> {
+    let src_state = crate::source_postgres::from_pool(&pool).await?;
+    let end_statements = schema::app_schema(src)?;
+    let end_state: anyhow::Result<Vec<Wrapped>> = end_statements
+        .clone()
+        .into_iter()
+        .map(|s| Wrapped::try_from(s))
+        .collect();
+    let end_state = end_state?;
+    let steps = crate::altertable::from_to(src_state, end_state)?;
+
+    let mut conn = pool.acquire().await?;
+    sqlx::query("SET lock_timeout TO 5000").execute(&mut *conn).await?;
+    for s in steps {
+        sqlx::query(&s.to_string()).execute(&mut *conn).await?;
+    }
+    Ok(())
+}
