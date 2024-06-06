0534f2ca0375627c6cc7506719dfb00d17e462d4
Philip Cristiano <git@philipcristiano.com>
2024-06-06 10:30:31 -0400

    feat: Expose way to generate migraitons but not run them

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,20 @@ pub async fn migrate_from_string(src: &String, pool: &PgPool) -> anyhow::Result<
     }
     Ok(())
 }
+
+pub async fn generate_migrations_from_string(
+    src: &String,
+    pool: &PgPool,
+) -> anyhow::Result<Vec<String>> {
+    let src_state = crate::source_postgres::from_pool(&pool).await?;
+    let end_statements = schema::app_schema(src)?;
+    let end_state: anyhow::Result<Vec<Wrapped>> = end_statements
+        .clone()
+        .into_iter()
+        .map(|s| Wrapped::try_from(s))
+        .collect();
+    let end_state = end_state?;
+    let steps = crate::altertable::from_to(src_state, end_state)?;
+
+    Ok(steps.into_iter().map(|f| f.to_string()).collect())
+}
